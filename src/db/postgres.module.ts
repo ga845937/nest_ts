ef07fd9b7762f8b85d1baff5f1807b1ce113adc6
ga845937 <ga845937@gmail.com>
2024-07-16 14:33:35 +0800

    fix: fileName

--- src/db/postgres.module.ts
+++ src/db/postgres.module.ts
@@ -2,7 +2,7 @@ import type { ModuleMetadata, Provider } from "@nestjs/common";
 
 import { Module } from "@nestjs/common";
 
-import { postgresProvider } from "./postgres/postgres";
+import { postgresProvider } from "./postgres/postgres.provide";
 
 const metadata: ModuleMetadata = {
     providers: postgresProvider as Provider[],
